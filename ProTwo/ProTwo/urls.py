a234d45443e443d69c2c16394e958880000f86a9
nyinyizin <nyinyizin@isgm2.com>
2023-06-21 16:20:57 +0900

    feat : push level one session

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first_project project.
+URL configuration for ProTwo project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from first_app import views
+from appTwo import views
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("first_app/",include("first_app.urls")),
+    path("help/",include("appTwo.urls")),
     path("admin/", admin.site.urls),
 ]
